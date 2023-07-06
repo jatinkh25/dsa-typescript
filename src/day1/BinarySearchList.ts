34a669e1d41439e3af8b1cef7bee440ca06311e9
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-07-06 12:55:13 +0530

    binary search completed

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,1 +1,14 @@
-export default function bs_list(haystack: number[], needle: number): boolean {}
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0,
+        high = haystack.length - 1;
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+
+        if (haystack[mid] === needle) return true;
+        else if (haystack[mid] > needle) high = mid - 1;
+        else low = mid + 1;
+    }
+
+    return false;
+}
